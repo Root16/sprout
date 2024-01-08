3c43637fb5e2a2e7c39830d8868a54123aa05c83
JR Peters <jrpetersjr@gmail.com>
2024-01-08 17:15:52 -0500

    Updated method names Refactored

--- src/Root16.Sprout/Extensions/IEnumerableExtensions.cs
+++ src/Root16.Sprout/Extensions/IEnumerableExtensions.cs
@@ -14,38 +14,35 @@ public static class IEnumerableExtensions
         return returnedValues;
     }
 
-    internal async static IAsyncEnumerable<T> StreamFinishedTasksAllRunning<T>(this IList<Task<T>> tasks)
+    internal async static IAsyncEnumerable<T> StreamFinishedTasksAllRunning<T>(this IList<Task<T>> runningFunctions)
     {
-        while (tasks.Any())
+        while (runningFunctions.Any())
         {
-            var finishedTask = await Task.WhenAny(tasks);
-            tasks.Remove(finishedTask);
+            var finishedTask = await Task.WhenAny(runningFunctions);
+            runningFunctions.Remove(finishedTask);
             yield return await finishedTask;
         }
     }
 
-    internal async static IAsyncEnumerable<TOutput> StreamFinishedTasksWithSpecificAmount<TOutput>(this List<KeyValuePair<StepRegistration,Func<StepRegistration,Task<TOutput>>>> tasksToRun, int amount = default)
+    internal async static IAsyncEnumerable<TOutput> StreamFinishedTasksWithSpecificAmount<TOutput>(this List<KeyValuePair<StepRegistration,Func<StepRegistration,Task<TOutput>>>> functionsToRun, int maxConcurrentAmount = default)
     {
-        var runningTasks = new List<Task<TOutput>>();
-        var newTasks = new List<KeyValuePair<StepRegistration, Func<StepRegistration, Task<TOutput>>>>();
         //Start a specific amount of tasks and then remove them from the list of tasks to run
-        var TasksToRun = tasksToRun.Take(amount).ToList();
-        TasksToRun.ForEach((x) => tasksToRun.Remove(x));
-        runningTasks = TasksToRun.Select(x => x.Value(x.Key)).ToList();
+        var batchOfFunctionsToRun = functionsToRun.Take(maxConcurrentAmount).ToList();
+        batchOfFunctionsToRun.ForEach((x) => functionsToRun.Remove(x));
+        var runningFunctions = batchOfFunctionsToRun.Select(x => x.Value(x.Key)).ToList();
 
-        while (runningTasks.Any())
+        while (runningFunctions.Any())
         {
             //When a task is finished return the value and then remove the task from running tasks and then find as many new tasks to start as the amount will allow. Start them and then remove the tasks from tasks to run.
             //Returning first allows the calling method to add more to the tasks before we do another check
-            var finishedTask = await Task.WhenAny(runningTasks);
-            var value = await finishedTask;
-            yield return value;
-            runningTasks.Remove(finishedTask);
-            if (tasksToRun.Any())
+            var finishedFunction = await Task.WhenAny(runningFunctions);
+            yield return await finishedFunction;
+            runningFunctions.Remove(finishedFunction);
+            if (functionsToRun.Any())
             {
-                newTasks = tasksToRun.Take(amount - (runningTasks.Count)).ToList();
-                runningTasks.AddRange(newTasks.Select(x => x.Value(x.Key)));
-                newTasks.ForEach(x => tasksToRun.Remove(x));
+                var newBatchOfFunctionsToRun = functionsToRun.Take(maxConcurrentAmount - (runningFunctions.Count)).ToList();
+                runningFunctions.AddRange(newBatchOfFunctionsToRun.Select(x => x.Value(x.Key)));
+                newBatchOfFunctionsToRun.ForEach(x => functionsToRun.Remove(x));
             }
         }
     }
