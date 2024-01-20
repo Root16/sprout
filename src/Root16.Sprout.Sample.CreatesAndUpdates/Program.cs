2525a638145ef11fd7a8debe94ade94be97df10c
Corey O'Brien <cobrien@root16.com>
2024-01-20 11:24:21 -0600

    added and renamed orig sample

---
+++ src/Root16.Sprout.Sample.CreatesAndUpdates/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Root16.Sprout;
+using Root16.Sprout.DataSources;
+using Root16.Sprout.DataSources.Dataverse;
+using Root16.Sprout.DependencyInjection;
+using Root16.Sprout.Sample;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Configuration.AddUserSecrets<Program>();
+
+builder.Services.AddSprout();
+
+builder.Services.RegisterStep<CreateContactTestStep>();
+builder.Services.RegisterStep<UpdateContactTestStep>();
+builder.Services.AddDataverseDataSource("Dataverse");
+
+builder.Services.AddSingleton(
+    _ => new MemoryDataSource<CreateContact>(SampleData.GenerateCreateContactSampleData(amount: 250))
+);
+
+builder.Services.AddSingleton(
+    _ => new MemoryDataSource<UpdateContact>(SampleData.GenerateUpdateContactSampleData(amount: 250, startNumber: 225))
+);
+
+
+var host = builder.Build();
+host.Start();
+
+var runtime = host.Services.GetRequiredService<IIntegrationRuntime>();
+_ = await runtime.RunStepAsync<CreateContactTestStep>();
+
+//Only the overlapping amount should be updated in this step as the data operation is "Update".
+//In the case above it's set so that only 25 of the possible 250 entities are update
+_ = await runtime.RunStepAsync<UpdateContactTestStep>();
+
+Console.WriteLine("Sprout Sample Complete.");
---
+++ src/Root16.Sprout.Sample.CreatesAndUpdates/SampleData.cs
@@ -0,0 +1,29 @@
+﻿namespace Root16.Sprout.Sample;
+
+internal class SampleData
+{
+    public static IEnumerable<CreateContact> GenerateCreateContactSampleData(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            yield return new CreateContact
+            {
+                FirstName = $"TestFirstName{i}",
+                LastName = $"TestLastName{i}"
+            };
+        }
+    }
+
+    public static IEnumerable<UpdateContact> GenerateUpdateContactSampleData(int amount, int startNumber)
+    {
+        for (int i = startNumber; i < (amount + startNumber); i++)
+        {
+            yield return new UpdateContact
+            {
+                FirstName = $"TestFirstName{i}",
+                LastName = $"TestLastName{i}",
+                EmailAddress = $"Test{i}@test.com"
+            };
+        }
+    }
+}
