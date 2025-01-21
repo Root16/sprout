614f951d6c9ae84a8d76f703a40b4ad02e013aee
James Trieu <jtrieu@root16.com>
2025-01-21 11:49:10 -0600

    add sql datasource

---
+++ src/Root16.Sprout.SqlServer/DataSources/Sql/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Root16.Sprout.DataSources.Sql;
+
+public static partial class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddSqlDataSource(this IServiceCollection services, string connectionStringName)
+    {
+        services.TryAddSingleton<ISqlDataSourceFactory, SqlDataSourceFactory>();
+        return services.AddTransient(services =>
+        {
+            var factory = services.GetRequiredService<ISqlDataSourceFactory>();
+            return factory.CreateDataSource(connectionStringName);
+        });
+    }
+}
