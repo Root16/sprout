030d405bc83492dda4cb366a64547c65ffa3ce9f
James Trieu <jtrieu@root16.com>
2025-01-27 07:41:06 -0600

    add dictionary & string extensions

---
+++ src/Root16.Sprout/Extensions/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Root16.Sprout.Extensions;
+
+public static partial class StringExtensions
+{
+    public static string? ToMaxLength(this string? value, int maxLength)
+    {
+        return !string.IsNullOrEmpty(value)
+            ? new string(value.Take(maxLength).ToArray())
+            : null;
+    }
+
+    public static string? FormatForSharepoint(this string? name)
+    {
+        if (string.IsNullOrEmpty(name)) return null;
+        var replacedSPChars = "[\\~#%&*.{}/:<>?|\"]";
+        var formattedName = Regex.Replace(name.Trim(), replacedSPChars, "-").Replace(@"\", "-").Trim();
+
+        return formattedName;
+    }
+}
