fb07e6e8d0dae837db572b002d71d2c6494a5882
James Trieu <jtrieu@root16.com>
2025-04-04 14:01:53 -0500

    add BypassBusinessLogicExecution bypass flag

--- src/Root16.Sprout.Dataverse/DataSources/Dataverse/DataverseDataSourceFlags.cs
+++ src/Root16.Sprout.Dataverse/DataSources/Dataverse/DataverseDataSourceFlags.cs
@@ -2,6 +2,24 @@
 
 public static class DataverseDataSourceFlags
 {
+    /// <summary>
+    /// Bypass only synchronous logic. This optional parameter is supported, but not recommended. Use BypassBusinessLogicExecution with the CustomSync value to get the same result.
+    /// </summary>
     public static readonly string BypassCustomPluginExecution = nameof(BypassCustomPluginExecution);
+    /// <summary>
+    /// Bypass Power Automate Flows.
+    /// </summary>
     public static readonly string SuppressCallbackRegistrationExpanderJob = nameof(SuppressCallbackRegistrationExpanderJob);
+    /// <summary>
+    /// Flag for BypassBusinessLogicExecution. Bypass both synchronous and asynchronous custom logic, excluding Power Automate Flows.
+    /// </summary>
+    public static readonly string BypassBusinessLogicExecution = nameof(BypassBusinessLogicExecution);
+    /// <summary>
+    /// Flag for BypassBusinessLogicExecution. Bypass only asynchronous custom logic, excluding Power Automate Flows.
+    /// </summary>
+    public static readonly string BypassBusinessLogicExecutionAsync = nameof(BypassBusinessLogicExecutionAsync);
+    /// <summary>
+    /// Flag for BypassBusinessLogicExecution. Bypass only synchronous custom logic.
+    /// </summary>
+    public static readonly string BypassBusinessLogicExecutionSync = nameof(BypassBusinessLogicExecutionSync);
 }
